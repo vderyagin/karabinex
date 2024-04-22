649ed42871e194abff16af1c7d1d27c1d97e7523
Victor Deryagin <vderyagin@gmail.com>
2024-04-22 21:47:54 +0300

    put rules in separate file

--- lib/karabinex.ex
+++ lib/karabinex.ex
@@ -1,58 +1,26 @@
 defmodule Karabinex do
-  alias Karabinex.{Keymap, Config, Manipulator}
-
-  @spec definitions :: Keymap.spec()
-  def definitions do
-    %{
-      "H-x": %{
-        e: {:app, "Emacs"},
-        "H-e": {:sh, "PATH=/opt/homebrew/bin:$PATH emacsclient -c -a '' &"},
-        c: {:app, "Brave Browser"},
-        s: {:app, "Slack"},
-        a: {:app, "Anki"},
-        b: {:app, "Books"},
-        t: {:app, "Telegram Desktop"},
-        d: {:app, "Dash"},
-        m: {:sh, "pgrep mpv && open -a mpv || true"},
-        r: %{
-          c: {:raycast, "extensions/raycast/raycast/confetti", repeat: :key},
-          g: {:raycast, "extensions/josephschmitt/gif-search/search"},
-          t: {:raycast, "extensions/gebeto/translate/translate"},
-          b: {:raycast, "extensions/nhojb/brew/search"},
-          e: {:raycast, "extensions/raycast/emoji-symbols/search-emoji-symbols"}
-        }
-      },
-      "H-k": %{
-        c: {:quit, "Brave Browser"},
-        s: {:quit, "Slack"},
-        "H-s": {:sh, "killall -9 Slack"},
-        "H-e": {:sh, "killall -9 Emacs"},
-        a: {:quit, "Anki"},
-        b: {:quit, "Books"},
-        t: {:quit, "Telegram Desktop"},
-        d: {:quit, "Dash"}
-      }
-    }
-  end
-
-  def json do
-    %{
-      title: "Nested Emacs-like bindings",
-      rules: [
-        %{
-          description: "Nested Emacs-like bindings",
-          manipulators:
-            definitions()
-            |> Config.parse_definitions()
-            |> Enum.flat_map(&Manipulator.generate/1)
-        }
-      ]
-    }
-    |> Jason.encode!(pretty: true)
-  end
+  alias Karabinex.{Rules, Config, Manipulator}
 
   def write_config do
     {:ok, _} = Application.ensure_all_started(:karabinex)
-    File.write!("./karabiner.json", json())
+
+    manipulators =
+      Rules.rules()
+      |> Config.parse_definitions()
+      |> Enum.flat_map(&Manipulator.generate/1)
+
+    File.write!(
+      "./karabiner.json",
+      %{
+        title: "Nested Emacs-like bindings",
+        rules: [
+          %{
+            description: "Nested Emacs-like bindings",
+            manipulators: manipulators
+          }
+        ]
+      }
+      |> Jason.encode!(pretty: true)
+    )
   end
 end
---
+++ lib/karabinex/rules.ex
@@ -0,0 +1,8 @@
+defmodule Karabinex do
+  alias Karabinex.{Keymap, Config, Manipulator}
+
+  @spec definitions :: Keymap.spec()
+  def rules do
+    %{}
+  end
+end
