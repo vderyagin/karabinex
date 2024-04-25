aa00eeae37a599f6eb0e4670152d6b03e08b291a
Victor Deryagin <vderyagin@gmail.com>
2024-04-25 23:39:22 +0300

    rename, fix err

--- lib/karabinex/config.ex
+++ lib/karabinex/config.ex
@@ -1,22 +1,21 @@
 defmodule Karabinex.Config do
   alias Karabinex.{Key, Keymap, Command, Chord}
 
-  def parse_definitions(defs, prefix_chord \\ Chord.new()) do
-    Enum.map(defs, &parse_definition(&1, prefix_chord))
+  def parse_definitions(defs, prefix \\ Chord.new()) do
+    Enum.map(defs, &parse_definition(&1, prefix))
   end
 
-  def parse_definition({key, %{} = keymap_spec}, prefix_chord) do
-    prefix_chord
-    |> Chord.append(Key.new(key))
-    Keymap.new(parse_definitions(keymap_spec, chord))
+  def parse_definition({key, %{} = keymap_spec}, prefix) do
+    chord = Chord.append(prefix, Key.new(key))
+    Keymap.new(chord, parse_definitions(keymap_spec, chord))
   end
 
-  def parse_definition({key, {kind, arg}}, prefix_chord) do
-    parse_definition({key, {kind, arg, []}}, prefix_chord)
+  def parse_definition({key, {kind, arg}}, prefix) do
+    parse_definition({key, {kind, arg, []}}, prefix)
   end
 
-  def parse_definition({key, {kind, arg, opts}}, prefix_chord) do
-    prefix_chord
+  def parse_definition({key, {kind, arg, opts}}, prefix) do
+    prefix
     |> Chord.append(Key.new(key))
     |> Command.new(kind, arg, opts)
   end
